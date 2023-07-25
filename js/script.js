a4543fcc35b6827ab533baf116d80200672ff503
atmoo-wil <wilaga.atmo@gmail.com>
2023-07-25 23:23:32 +0700

    fiksasi laman contact dan terintegrasi dengan g-sheets

--- js/script.js
+++ js/script.js
@@ -14,3 +14,31 @@ document.addEventListener("click", function (e) {
     namaMenu.classList.remove("active");
   }
 });
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyCcqulszlWLVLo5RusL3LBDA5A1Rx4tvSL-4hoZZ-pkBXM8KrWxztvud6sAZ0DjpTYbA/exec";
+const form = document.forms["lemaloka-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const alert = document.querySelector(".alert-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika klik kirim
+  // tampilkan tombol loading & hilangkan tombol kirim
+  btnKirim.classList.toggle("btn-loading");
+  btnLoading.classList.toggle("btn-loading");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnKirim.classList.toggle("btn-loading");
+      btnLoading.classList.toggle("btn-loading");
+      alert.classList.toggle("alert-none");
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+document.querySelector(".closebtn").onclick = () => {
+  alert.classList.toggle("alert-none");
+};
